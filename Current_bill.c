29e31c015070192b77678e04ddd7734c6d78e78f
arshibegum <147418400+arshibegum@users.noreply.github.com>
2023-12-24 11:33:51 +0530

    #include<stdio.h> int main() {     int u;     float tb,b;     scanf("%d",&u);     if(u<=199)     {         b=u*1.20;     }     else if(u>=200&&u<400)     {         b=u*1.50;     }     else     {         b=u*2.00;     }     if(b>400)     {         tb=b+b*0.15;         printf("%.2f",tb);     }     else     {         tb=b+100;         printf("%.2f",tb);
        }
    }

---
+++ Current_bill.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int u;
+    float tb,b;
+    scanf("%d",&u);
+    if(u<=199)
+    {
+        b=u*1.20;
+    }
+    else if(u>=200&&u<400)
+    {
+        b=u*1.50;
+    }
+    else
+    {
+        b=u*2.00;
+    }
+    if(b>400)
+    {
+        tb=b+b*0.15;
+        printf("%.2f",tb);
+    }
+    else
+    {
+        tb=b+100;
+        printf("%.2f",tb);
+        
+    }
+}
